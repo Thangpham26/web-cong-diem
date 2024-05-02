9b2a195b76ac352e7336a5bd82d87cd178630ce4
Thangpham26 <125008440+Thangpham26@users.noreply.github.com>
2024-05-02 21:10:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function submit(){
+    const email = document.getElementById("exampleFormControlInput1").value;
+    const text = document.getElementById("exampleFormControlTextarea1").value;
+    window.alert("Tôi sẽ phản hồi với bạn qua email " + email)
+
+}
